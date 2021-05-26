94000969a7750631f3b9c402a33871084b788aa0
Juan Pablo <juanpablogast@gmail.com>
2021-05-25 21:57:57 -0400

    modificando la tabla intermedia

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_011534) do
+ActiveRecord::Schema.define(version: 2021_05_26_015405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "post_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.boolean "available", default: true
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,11 +27,6 @@ ActiveRecord::Schema.define(version: 2021_05_26_011534) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "posts_tags", id: false, force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "tag_id", null: false
   end
 
   create_table "tags", force: :cascade do |t|

feeefcd5f615482ae087d6628eccd22d6701978e
Juan Pablo <juanpablogast@gmail.com>
2021-05-27 21:18:18 -0400

    implementando devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :post_tags
 
+  root to: "posts#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
